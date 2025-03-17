d202fb4080c373789573b58abb71132e13f2514c
Florian <florian@poissonmail.fr>
2025-03-17 14:48:57 +0000

    films populaires avec env secret api key tmdb

---
+++ config/secrets/dev/dev.list.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'TMDB_API' => null,
+];
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,30 +3,47 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\CacheItem;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\Cache\CacheInterface;
 
 final class HomeController extends AbstractController
 {
     public function __construct(
         private HttpClientInterface $client,
+        private CacheInterface $cache,
+        private ParameterBagInterface $parameterBag
     ) {
     }
 
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        $response = $this->client->request('GET', 'https://api.themoviedb.org/3/trending/movie/day?language=fr-FR', [
-          'headers' => [
-            'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZTg4NGI4NDJmNTJiYzkwYTg5MmQwY2MxY2E2YmY5OSIsIm5iZiI6MTY3ODYwNzgzMy42Nywic3ViIjoiNjQwZDg1ZDkzMjNlYmEwMTBiOTA4NzJmIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.LkYcIcrF3U8dof4X6mgN_Vmd6MhCvdhLuopnsEiFSBE',
-            'accept' => 'application/json',
-          ],
-        ]);
+        // Vérifier si les films populaires sont déjà en cache
+        $cachedMovies = $this->cache->get('trending_movies', function (CacheItem $item) {
+            // Configurer la durée de vie du cache ici (par exemple, 1 heure)
+            $item->expiresAfter($this->parameterBag->get('TMDB_EXPIRES_AFTER')); // 1 semaine
+
+            // Faire l'appel à l'API TMDB
+            $response = $this->client->request('GET', 'https://api.themoviedb.org/3/trending/movie/day?language=fr-FR', [
+                'headers' => [
+                  'Authorization' => 'Bearer ' . $this->parameterBag->get('tmdb_api'),
+                  'accept' => 'application/json',
+                ],
+              ]);
+
+            $data = json_decode($response->getContent());
+
+            // Retourner les films populaires
+            return $data->results;
+        });
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'popular_movies' => json_decode($response->getContent()),
+            'popular_movies' => $cachedMovies,
         ]);
     }
 }
